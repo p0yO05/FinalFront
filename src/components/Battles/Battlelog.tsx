6caea6eec4cc1d01d72d5c92809f71d50f98a062
Pablo T <licenciadotyr@gmailcom>
2025-05-19 18:43:39 -0500

    cambios

--- src/components/Battles/Battlelog.tsx
+++ src/components/Battles/Battlelog.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Battle } from '../../types/types';
-import { generateBattleNarration } from '../../utils/BattleAI';
+
 interface BattleLogProps {
   battles: Battle[];
 }
